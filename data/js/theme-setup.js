9765285daeb3f1bdf60459b2de69383170c50b9c
arg-min-h <amplitude.modulation.housou@gmail.com>
2024-07-14 23:42:28 +0900

    PWAでホーム画面から接続したときにシステムのテーマ設定が反映されない不具合の修正。

--- data/js/theme-setup.js
+++ data/js/theme-setup.js
@@ -1,11 +1,17 @@
-// ページ読み込み時にシステムテーマを取得してbodyのクラスを設定する
 document.addEventListener("DOMContentLoaded", function () {
   const prefersDarkScheme = window.matchMedia(
     "(prefers-color-scheme: dark)",
   ).matches;
-  const body = document.body;
-  body.classList.toggle("dark-theme", prefersDarkScheme);
-  body.classList.toggle("light-theme", !prefersDarkScheme);
+  const savedTheme = localStorage.getItem("theme");
+
+  if (savedTheme) {
+    document.body.classList.toggle("dark", savedTheme === "dark");
+    document.body.classList.toggle("light", savedTheme === "light");
+  } else {
+    document.body.classList.toggle("dark", prefersDarkScheme);
+    document.body.classList.toggle("light", !prefersDarkScheme);
+  }
+
   document
     .querySelector('meta[name="theme-color"]')
     .setAttribute("content", prefersDarkScheme ? "#333333" : "#ffffff");
